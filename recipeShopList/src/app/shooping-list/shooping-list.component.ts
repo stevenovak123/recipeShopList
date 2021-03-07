714e317e6cfb3d191a8095c558ac0b9bfb90a10f
stevenovak123 <stevenovak0802@gmail.com>
2021-03-07 23:56:58 +0530

    added functionality to shopping component

--- recipeShopList/src/app/shooping-list/shooping-list.component.ts
+++ recipeShopList/src/app/shooping-list/shooping-list.component.ts
@@ -1,3 +1,4 @@
+import { Ingredient } from './../shared/ingredient.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shooping-list.component.css']
 })
 export class ShoopingListComponent implements OnInit {
-
+ingredients: Ingredient[] =[ 
+  new Ingredient('Apples',5),
+  new Ingredient('Tomatoes',5),
+];
   constructor() { }
 
   ngOnInit(): void {
