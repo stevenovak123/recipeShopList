df1d4383b5816529ae89a6bd4c6f809aa11fcb65
stevenovak123 <stevenovak0802@gmail.com>
2021-03-11 00:43:42 +0530

    added basic navigation

--- recipeShopList/src/app/app.component.ts
+++ recipeShopList/src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'recipeShopList';
+  loadedFeature='recipe';
+
+  onNavigate(feature:string){
+    this.loadedFeature=feature;
+  }
 }
