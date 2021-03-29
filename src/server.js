bf23f1eea356a12b58df6edd07dbb737a4c03c37
stevenovak123 <stevenovak0802@gmail.com>
2021-03-29 14:23:48 +0530

    heroku

--- src/server.js
+++ src/server.js
@@ -5,4 +5,4 @@ app.use(express.static(__dirname + '/dist/recipe-shop-list'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
 '/dist/recipeShopList/index.html'));});
-app.listen(process.env.PORT || 8000);
+app.listen(process.env.PORT || 8080);
