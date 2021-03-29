813c32e35030255cb8c675e21878b90d02c77fcb
stevenovak123 <stevenovak0802@gmail.com>
2021-03-29 16:00:58 +0530

    Heroku bug fixes

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const app = express();
 app.use(express.static(__dirname + '/dist/recipe-shop-list'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/recipeShopList/index.html'));});
+'/dist/recipeShopList/'));});
 app.listen(process.env.PORT || 8080);
