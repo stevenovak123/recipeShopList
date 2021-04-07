b9136ba295e758e2b7b99e3d04f2f574f66e49bf
stevenovak123 <stevenovak0802@gmail.com>
2021-04-07 15:52:29 +0530

    indentations

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express');
+
 const path = require('path');
+
 const app = express();
+
 app.use(express.static(__dirname + '/dist/recipeShopList'));
-app.get('/*', function(req,res) {
+
+app.get('/*', function(req,res)
+ {
 res.sendFile(path.join(__dirname+
-'/dist/recipeShopList/index.html'));});
+'/dist/recipeShopList/index.html'));
+}
+);
+
+
 app.listen(process.env.PORT || 8080);
